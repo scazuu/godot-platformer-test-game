2160197b5e7d423e4cd9b5629b50b7f494ed34d8
scazuu <zafferahm@gmail.com>
2026-08-31 10:48:44 +0530

    style player script
    + removed whitespaces and template comments
    + increased z index for player sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,13 +9,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if (Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("jump")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+
 	# Handle movement.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		print("walking")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
