c519bc4c0c2b8031effd96a63e34fc81c1837c65
scazuu <zafferahm@gmail.com>
2026-08-31 12:16:38 +0530

    update player script for animations
    + animated sprite node has more animations, run and jump
    + player collision layer set to 2 now

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
+
+@onready var player_animatedSprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,10 +15,28 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Handle movement.
+	# gets direction - it will either be a 1 or -1, defaults at start is 0.
 	var direction := Input.get_axis("move_left", "move_right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Handles sprites.
+	# flips sprite on direction
+	if direction > 0:
+		player_animatedSprite.flip_h = false
+	elif direction < 0:
+		player_animatedSprite.flip_h = true
+
+	# changes animations based on actions
+	if is_on_floor():
+		if direction == 0:
+			player_animatedSprite.play("default")
+		else:
+			player_animatedSprite.play("run")
+	else:
+		player_animatedSprite.play("jump")
 
 	move_and_slide()
