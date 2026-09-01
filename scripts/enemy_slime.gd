5883edfc7a07c021eec35a727700e865dbb00861
scazuu <zafferahm@gmail.com>
2026-09-01 16:32:37 +0530

    update enemy slime with sprite flips
    + script flips sprite
    + node updated with more frames in animation

--- scripts/enemy_slime.gd
+++ scripts/enemy_slime.gd
@@ -5,11 +5,14 @@ var direction = 1
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta):
 	if ray_cast_left.is_colliding():
+		animated_sprite_2d.flip_h = false
 		direction = 1
 	if ray_cast_right.is_colliding():
+		animated_sprite_2d.flip_h = true
 		direction = -1
-		
+
 	position.x += direction * speed * delta
