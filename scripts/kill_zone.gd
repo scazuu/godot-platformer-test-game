1fca538119feda1836d00fe8c81e0c56d7f4310c
scazuu <zafferahm@gmail.com>
2026-09-01 16:37:19 +0530

    update killzone script with quality of life
    + player death bool refered here
    + game time to slow down on death and reset on reload of scene
    + player body gets knocked up when death happens, also knock up const declared instead of a magic number

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+const KNOCK_UP = -200
 
 func _on_body_entered(_body):
 	print("Died")
+	_body.IS_DEAD = true
+	Engine.time_scale = 0.5
+	_body.get_node("CollisionShape2D").queue_free()
+	_body.velocity.y = KNOCK_UP
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
